50dc252780c871442969112790ddb9a5e81d6672
William Bae <wbae912@gmail.com>
2019-11-19 12:08:17 -0800

    Working on POST /guess endpoint

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,11 +1,14 @@
+/* eslint-disable quotes */
 const express = require('express');
 const LanguageService = require('./language-service');
 const { requireAuth } = require('../middleware/jwt-auth');
 
 const languageRouter = express.Router();
+const jsonParser = express.json();
 
 languageRouter
   .use(requireAuth)
+  .use(jsonParser)
   .use(async (req, res, next) => {
     try {
       const language = await LanguageService.getUsersLanguage(
@@ -64,8 +67,46 @@ languageRouter
 
 languageRouter
   .post('/guess', async (req, res, next) => {
-    // implement me
-    res.send('implement me!');
+    try{
+      const db = req.app.get('db');
+      const language_id = req.language.id;
+
+      const words = await LanguageService.getLanguageWords(db, language_id);
+      const list = await LanguageService.populateLinkedList(words);
+
+      const { guess } = req.body;
+  
+      if(!guess) {
+        return res.status(400).json( {error: `Missing 'guess' in request body`} );
+      }
+
+      const language = req.language;
+
+
+      LanguageService.memoryAlgorithm(list, guess, language, res);
+
+      next();
+    } catch(error) {
+      next(error);
+    }
+
+    // res.send('implement me!');
   });
 
 module.exports = languageRouter;
+
+
+// Question:       1   2   3   4   5
+
+// Memory_value:   1   1   1   1   1
+
+
+// Question:       2   3   1   4   5
+
+// Memory_value:   1   1   2   1   1
+
+
+//Is the head going to constantly change? Do we need to look at the other "nodes" in our Linked List
+//Is the correct approach to use the remove() and insertAt() methods in the Linked List and insertAt("M")?
+//Are we only looking at the head, checking to see if the user's input === head.translation?
+//What does it mean by move back the question "M" places in the list?? What happens if "M" is 1000, for example...
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,3 +1,5 @@
+const { LinkedList } = require('../../Utils/LinkedList');
+
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
@@ -41,7 +43,67 @@ const LanguageService = {
           wordIncorrectCount: word[0].incorrect_count
         };
       });
+  },
+
+  populateLinkedList(array) {
+    const list = new LinkedList();
+    if(list.head === null) {
+      list.insertFirst(array[0]);
+    }
+
+    let newArray = array.slice(1);
+    newArray.map(word => {
+      list.insertLast(word);
+    });
+
+    return list;
+  },
+
+  memoryAlgorithm(list, userAnswer, language, res) {
+    const head = list.head.value;
+    let M = head.memory_value;
+
+    if(head.translation === userAnswer) {
+      M *= 2;
+      // head.correct_count++;
+      language.total_score++;
+
+      const removedHead = list.head.value;
+
+      list.remove(head);
+
+      list.insertAt(removedHead, M);
+
+      return res.status(200).json({
+        nextWord: list.head.value.original,
+        totalScore: language.total_score,
+        wordCorrectCount: removedHead.correct_count,
+        wordIncorrectCount: removedHead.incorrect_count,
+        answer: removedHead.translation,
+        isCorrect: true
+      });
+
+    } else {
+      M = 1;
+      // head.incorrect_count++;
+
+      const removedHead = list.head.value;
+
+      list.remove(head);
+
+      list.insertAt(removedHead, M);
+
+      return res.status(200).json({
+        nextWord: list.head.value.original,
+        totalScore: language.total_score,
+        wordCorrectCount: removedHead.correct_count,
+        wordIncorrectCount: removedHead.incorrect_count,
+        answer: removedHead.translation,
+        isCorrect: false
+      });
+    }
   }
+
 };
 
 module.exports = LanguageService;
