f84133888f8b7c620db23a08bb614264df97c916
William Bae <wbae912@gmail.com>
2019-11-19 07:18:11 -0800

    Fixed all ESLINT errors

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@ module.exports = {
     || 'postgresql://joeyromo@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-}
+};
